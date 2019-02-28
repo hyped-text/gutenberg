6ae007a6d626b86382864774dcb0c306fbe75a7e
shonie <shonie.starnikov@gmail.com>
2019-02-28 03:26:51 +0200

    Implemented dataset loading

--- download.sh
+++ download.sh
@@ -4,35 +4,32 @@ SOURCE=http://www.gutenberg.org/cache/epub/feeds/rdf-files.tar.zip
 
 ZIP_FILE=rdf-files.tar.zip
 
-CACHE_DIR=$HOME/.gutenberg
+CACHE_DIR=$PWD
 
 if [ -d $CACHE_DIR/cache ]; then
   echo Found cached resources, nothing to download, exiting
   exit 0
 fi
 
-echo Downloading gutenberg archive from $SOURCE
+echo Downloading gutenberg catalog from $SOURCE
 
-curl $SOURCE --output $CACHE_DIR/$ZIP_FILE
+wget -O $CACHE_DIR/$ZIP_FILE $SOURCE
 
-echo Downloading is complete!
+echo Catalog downloaded!
 
 if ! [ -x "$(command -v unzip)" ]; then
   echo Installing unzip
-  sudo apt-get update
-  sudo apt-get install -y unzip
+  apk update
+  apk add unzip
 fi
 
 echo Unzipping archive
 
-unzip -u -d $CACHE_DIR $CACHE_DIR/$ZIP_FILE
-
-echo Unzipping is complete!
-
-echo Untaring nested archive
+unzip -d $CACHE_DIR $CACHE_DIR/$ZIP_FILE
 
 tar --extract -C $CACHE_DIR -f $CACHE_DIR/rdf-files.tar
   
-echo Untaring is complete!
+echo Unzipped to $CACHE_DIR/cache
 
-exit 0
+echo Done!
+
